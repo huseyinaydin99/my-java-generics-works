c20c4954022a2171f8fec98a99d21ec4d0635fee
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-11-08 13:43:53 +0300

    Jenerik parametrede belirli türü extends/implements etme şartı.
    countGreaterThan metodunda bounded type parameter kullanarak, tür parametresi T'nin, Comparable<T> arayüzünü genişletmesini istedim. Bu, sadece compareTo metodunu implement eden türlerin geçerli olmasını sağlar. Yani, T türündeki öğeler, birbirleriyle karşılaştırılabilir olmalı.
    
    countGreaterThan metodu, verilen dizideki elemanları, belirli bir sayıyı aşan değerleri sayarak döndürüyor. Yani, dizideki her elemanla number'ı karşılaştırarak greater than (büyüktür) koşulunu kontrol ediyor.
    
    Integer[] numbers ve Double[] numbers2 dizileri üzerinde bu metodu çalıştırdım. İlk olarak, Integer dizisini ve ardından Double dizisini, her birinin uygun değerini (sırasıyla 0 ve 0.9) kullanarak karşılaştırma yaptım.
    
    Kodda A ve B adında iki boş arayüz (interface) tanımladım, ancak bu arayüzler şu anda kullanılmıyor. Bunlar sadece örnek olarak gösterildi.

---
+++ src/main/java/tr/com/huseyinaydin/example008/BoundedTypeParameter.java
@@ -0,0 +1,38 @@
+package tr.com.huseyinaydin.example008;
+
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java Generics
+ *
+ */
+
+public class BoundedTypeParameter {
+    public static void main(String[] args) {
+        Integer[] numbers = {1, 2, 3};
+        System.out.println(
+                countGreaterThan(numbers, 0)
+        );
+
+        Double[] numbers2 = {1.0, 2.0, 3.8};
+        System.out.println(
+                countGreaterThan(numbers2, .9)
+        );
+    }
+
+    interface A {}
+    interface B {}
+
+    static <T extends Comparable<T>> int countGreaterThan(T[] numbers, T number) {
+        int count = 0;
+        for (T n : numbers) {
+            if (n.compareTo(number) > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
